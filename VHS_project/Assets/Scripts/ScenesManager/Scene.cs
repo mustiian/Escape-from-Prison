46d05aa36a3065c88f76ce486d293790a4c0cd64
Ian Mustiats <mustiian@fit.cvut.cz>
2019-11-23 16:35:03 +0100

    Create dummy scene manager

---
+++ VHS_project/Assets/Scripts/ScenesManager/Scene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class Scene : MonoBehaviour
+{
+    public string Name;
+    public Transform respawnPosition;
+    public Quaternion respawnRotation;
+
+    private GameObject player;
+    private GameObject fader;
+    private FirstPersonController playerController;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerController = player.GetComponent<FirstPersonController> ();
+        fader = GameObject.FindGameObjectWithTag ("Fader");
+    }
+
+    public void ChangeScene()
+    {
+        playerController.enabled = false;
+        StartCoroutine (changeSceneEnumerator ());
+    }
+
+    private IEnumerator changeSceneEnumerator()
+    {
+        fader.GetComponent<Animator> ().SetTrigger ("fadein");
+
+        yield return new WaitForSeconds (1f);
+
+        player.transform.rotation = respawnRotation;
+        player.transform.position = respawnPosition.position;
+
+        fader.GetComponent<Animator> ().SetTrigger ("fadeout");
+
+        yield return new WaitForSeconds (0.01f);
+
+        playerController.enabled = true;
+        playerController.SetMouseCursor(true);
+    }
+}
---
+++ VHS_project/Assets/Scripts/ScenesManager/SceneChangeTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    private GameObject player;
+    private FirstPersonController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerController = player.GetComponent<FirstPersonController> ();
+    }
+
+    private void Update()
+    {
+        if (IsCloseToObject(player, 2) && Input.GetKeyDown(KeyCode.E))
+        {
+            playerController.SetMouseCursor (false);
+        }
+    }
+
+    private bool IsCloseToObject( GameObject target, float minDistance = 1 )
+    {
+        Vector3 gap = target.transform.position - transform.position;
+        float distance = gap.sqrMagnitude;
+
+        if (distance < minDistance * minDistance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
