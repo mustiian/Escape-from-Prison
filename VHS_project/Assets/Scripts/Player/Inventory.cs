0d471f8c81fb6f10511cfed23a93039a4c7b667e
Ian Mustiats <mustiian@fit.cvut.cz>
2019-12-29 15:53:58 +0100

    Add new models. Add items and inventory system.

---
+++ VHS_project/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item ActiveItem;
+
+    public bool IsItemEquipped = false;
+
+    public Transform Hands;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsItemEquipped && Input.GetKeyDown (KeyCode.E))
+        {
+            DropItem ();
+        }
+    }
+
+    public void EquipItem(Item item)
+    {
+        if (IsCloseToObject(item.gameObject, 2))
+        {
+            ActiveItem = item;
+            IsItemEquipped = true;
+
+            ActiveItem.GetComponent<Rigidbody> ().useGravity = false;
+            ActiveItem.transform.position = new Vector3 (0, 500, 0);
+            ActiveItem.transform.parent = Hands;
+        }
+    }
+
+    public void DropItem()
+    {
+        IsItemEquipped = false;
+
+        ActiveItem.GetComponent<Rigidbody> ().useGravity = true;
+        ActiveItem.transform.position = Hands.position;
+        ActiveItem.transform.parent = null;
+
+        ActiveItem = null;
+    }
+
+    private bool IsCloseToObject( GameObject target, float minDistance = 1 )
+    {
+        Vector3 gap = target.transform.position - transform.position;
+        float distance = gap.sqrMagnitude;
+
+        if (distance < minDistance * minDistance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
--- VHS_project/Assets/Scripts/Player/PlayerRaycast.cs
+++ VHS_project/Assets/Scripts/Player/PlayerRaycast.cs
@@ -7,10 +7,13 @@ public class PlayerRaycast : MonoBehaviour
 {
     private Camera mainCamera;
 
+    private Inventory playerInventory;
+
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+        playerInventory = GameObject.FindGameObjectWithTag ("Player").GetComponent<Inventory> ();
     }
 
     // Update is called once per frame
@@ -30,7 +33,21 @@ public class PlayerRaycast : MonoBehaviour
                 }
             }
         }
-    }
 
-    
+        if (Input.GetKeyDown (KeyCode.E))
+        {
+            var ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast (ray, out hit))
+            {
+                Item item = hit.transform.gameObject.GetComponent<Item> ();
+
+                if (item != null && !playerInventory.IsItemEquipped)
+                {
+                    playerInventory.EquipItem (item);
+                }
+            }
+        }
+    }
 }
