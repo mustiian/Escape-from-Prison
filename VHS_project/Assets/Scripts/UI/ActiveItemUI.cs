76839538ca8e531a53216e23893cdf328df91368
Ian Mustiats <mustiian@fit.cvut.cz>
2020-01-17 19:23:45 +0100

    Add crosshair for active items

---
+++ VHS_project/Assets/Scripts/UI/ActiveItemUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveItemUI : MonoBehaviour
+{
+    public Image CrosshairImage;
+
+    public Sprite DefaultSprite;
+
+    public Sprite ChangedSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        CrosshairImage.sprite = ChangedSprite;
+    }
+
+    private void OnMouseExit()
+    {
+        CrosshairImage.sprite = DefaultSprite;
+    }
+}
